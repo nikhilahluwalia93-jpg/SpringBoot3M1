9a929dd37fbdf7d2d760c3129b20f2e1d8e79386
Nikhil Walia <nikhilwalia@Nikhils-MacBook-Air.local>
2026-05-24 08:25:31 +0530

    Initial commit

---
+++ src/main/java/com/example/cones/SpringBoot3M1/SpringBoot3M1Application.java
@@ -0,0 +1,13 @@
+package com.example.cones.SpringBoot3M1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3M1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3M1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cones/SpringBoot3M1/SpringBoot3M1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cones.SpringBoot3M1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3M1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
